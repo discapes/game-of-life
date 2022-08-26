37ce8f9268dbd021fe7a1c1bf492b11b2ecc78b6
Miika Tuominen <miika.km.tuominen@gmail.com>
2022-08-26 22:05:27 +0300

    optimization and infinite field

--- src/App.svelte
+++ src/App.svelte
@@ -1,76 +1,120 @@
 <script>
-  // @ts-nocheck
   import { onMount, onDestroy } from "svelte";
-  import { debug } from "svelte/internal";
 
-  const w = 10;
-  const h = 10;
-  let current = Array(w)
-    .fill(0)
-    .map((col) => Array(h).fill(0));
-  let next = Array(w)
-    .fill(0)
-    .map((col) => Array(h).fill(0));
-  let intervalID;
-  const intervalSeconds = 1;
+  const initialW = 10;
+  const initialH = 10;
+  let current = new Set();
+  let maxx = initialW;
+  let minx = -1;
+  let miny = -1;
+  let maxy = initialH;
+  const intervalSeconds = 0.01;
+  let stopped = false;
 
-  // onMount(() => setInterval(iter, intervalSeconds * 1000));
-  // onDestroy(() => clearInterval(intervalID));
+  function stop() {
+    stopped = true;
+  }
+
+  function play() {
+    stopped = false;
+    function f() {
+      if (stopped) return;
+      iter();
+      setTimeout(f, intervalSeconds * 1000);
+    }
+    f();
+  }
 
   function iter() {
-    console.log("iterating..");
-    for (let x = 0; x < current.length; x++) {
-      for (let y = 0; y < current.length; y++) {
-        changeCell(x, y);
-      }
-    }
-    const old = current;
-    current = next;
-    next = old;
-  }
-  function changeCell(x, y) {
-    let neighbours = 0;
-    const minx = !x - 1;
-    const miny = !y - 1;
-    const maxx = !!(w - x - 1);
-    const maxy = !!(h - y - 1);
+    minx = miny = Infinity;
+    maxx = maxy = -Infinity;
 
-    for (let dx = minx; dx <= maxx; dx++) {
-      for (let dy = miny; dy <= maxy; dy++) {
-        if (dx === 0 && dy === 0) continue;
-        neighbours += current[x + dx][y + dy];
+    console.log("iterating..");
+    const next = new Set();
+    const deadNeighbours = new Set();
+    for (const s of current.values()) {
+      const [x, y] = s.split(",").map((s) => +s);
+      let neighbours = 0;
+      forNeighbours(x, y, (nx, ny) => {
+        if (current.has([nx, ny].join(","))) neighbours++;
+        else deadNeighbours.add([nx, ny].join(","));
+      });
+      if (neighbours === 2 || neighbours === 3) {
+        next.add([x, y].join(","));
+        minx = Math.min(x, minx);
+        miny = Math.min(y, miny);
+        maxx = Math.max(x, maxx);
+        maxy = Math.max(y, maxy);
       }
     }
-    switch (neighbours) {
-      case 3:
-        next[x][y] = 1;
-        break;
-      case 2:
-        next[x][y] = current[x][y];
-        break;
-      default:
-        next[x][y] = 0;
-        break;
+
+    for (const s of deadNeighbours.values()) {
+      const [x, y] = s.split(",").map((s) => +s);
+      let neighbours = 0;
+      forNeighbours(x, y, (nx, ny) => {
+        neighbours += +current.has([nx, ny].join(","));
+      });
+      if (neighbours === 3) {
+        next.add([x, y].join(","));
+        minx = Math.min(x, minx);
+        miny = Math.min(y, miny);
+        maxx = Math.max(x, maxx);
+        maxy = Math.max(y, maxy);
+      }
+    }
+    minx--;
+    miny--;
+    maxx++;
+    maxy++;
+    current = next;
+  }
+  function forNeighbours(x, y, cb) {
+    for (let dx = -1; dx <= 1; dx++) {
+      for (let dy = -1; dy <= 1; dy++) {
+        if (dx === 0 && dy === 0) continue;
+        cb(x + dx, y + dy);
+      }
     }
   }
 </script>
 
-<main class="flex flex-col justify-center items-center overflow-auto h-screen">
+<svelte:window on:keyup={(e) => e.key == " " && iter()} />
+
+<main
+  class="inline-flex min-w-full flex-col justify-center items-center h-screen">
   <div class="flex gap-5 p-5 items-center">
     <h1 class="text-xl">game of life</h1>
     <button on:click={iter}>Iterate</button>
+    <button on:click={play}>Play</button>
+    <button on:click={stop}>Stop</button>
   </div>
 
-  <div style="column-count: {w}" class="col gap-x-0">
-    {#each current as col, x}
-      {#each col as cell, y}
-        <div
-          class="border w-10 h-10 {cell
-            ? 'bg-white border-black'
-            : 'bg-black border-white'}"
-          on:click={() => (current[x][y] = !current[x][y])} />
+  <div class="w-full h-full flex justify-center">
+    <div
+      style="grid-template-columns: repeat({maxx -
+        minx +
+        1}, minmax(0, 1fr)); grid-template-rows: repeat({maxy -
+        miny +
+        1}, minmax(0, 1fr)); 
+      width: min(100%, {(maxx - minx + 1) * 40}px);
+      height: min(100%, {(maxy - miny + 1) * 40}px)"
+      class="inline-grid gap-x-0 p-10 aspect-square">
+      {#each Array(maxy - miny + 1).fill(0) as _y, y}
+        {#each Array(maxx - minx + 1).fill(0) as _x, x}
+          <div
+            class="w-full h-full max-w-[40px] max-h-[40px] {current.has(
+              [x + minx, y + miny].join(',')
+            )
+              ? 'bg-white border-black'
+              : 'bg-black border-white'}"
+            on:click={() => {
+              if (!current.delete([x + minx, y + miny].join(",")))
+                current.add([x + minx, y + miny].join(","));
+              current = current;
+            }} />
+        {/each}
       {/each}
-    {/each}
+    </div>
   </div>
 </main>
 
